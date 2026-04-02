329b3c558380b66b344ec04511f58be65ce06354
prathmeshcool <89856936+prathmeshcool@users.noreply.github.com>
2026-04-02 12:36:50 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,38 @@
+class TimeMap {
+public:
+    unordered_map<string,vector<pair<int,string>>> mp;
+    TimeMap() {
+        mp.clear();
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        
+        if(mp.find(key)==mp.end())
+            return "";
+
+        auto &v = mp[key];
+
+        int n = v.size();
+
+        int l=0, r=n-1;
+        string ans="";
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            auto [t, val]=v[mid];
+            if(t==timestamp){
+                return val;
+            }
+            else if(t<timestamp){
+                ans=val;
+                l=mid+1;
+            }
+            else
+                r=mid-1;
+        }
+        return ans;
+    }
+};
